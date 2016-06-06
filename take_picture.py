6dac94fa56556f623232df6c499cc90b3bd6e2ef
programJ <jhaveripranjal@gmail.com>
2016-06-06 13:48:23 +0530

    Update take_picture.py

--- take_picture.py
+++ take_picture.py
@@ -5,10 +5,6 @@ import os
 
 t=time.asctime(time.localtime(time.time()))
 
-
-#newpath = '/home/pranjal/iitbombay/extracurriculam activities/ITSP/photo/Session_'+ str(t)
-#if not os.path.exists(newpath):
-#    os.makedirs(newpath)
 cap=cv2.VideoCapture(0)
 
 j=0
@@ -19,7 +15,7 @@ while(True):
 	if k== ord('q'):
 		break
 	if k== ord('s'):
-		newpath = '/home/pranjal/iitbombay/extracurriculam activities/ITSP/photo/session_'+ str(t)
+		newpath = './photo/session_'+ str(t)
 		if not os.path.exists(newpath):
     			os.makedirs(newpath)
 		cv2.imwrite(newpath+ '/photo_'+ str(j)+ '.jpg', frame)
